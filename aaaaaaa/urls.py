a889222f73a2e464be399a386897629f1a754550
dch9610 <dch9610@naver.com>
2020-07-30 20:16:01 +0900

    Test

--- aaaaaaa/urls.py
+++ aaaaaaa/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hi/', admin.site.urls),
-    path('hello/', admin.site.urls)
+    path('hello/', admin.site.urls),
+    path('hello12132/', admin.site.urls)
 ]
